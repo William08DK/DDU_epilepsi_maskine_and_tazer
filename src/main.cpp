1fc4f1e0640a6ba8ee54e642a8f027b5d029fe1d
William Sørensen <williamfastsorensen@gmail.com>
2026-08-19 10:06:30 +0200

    Added 4 Stage control

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #endif
 
 #define LED_PIN   6
-#define button_1  2
+#define Stage_1 7
+#define Stage_2 8
+#define Stage_3 9
+#define Stage_4 10 
 
 #define LED_COUNT   60
 
@@ -16,7 +19,7 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(button_1, INPUT_PULLUP);
+  pinMode(Stage_1, INPUT_PULLUP);
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -31,25 +34,25 @@ void setup() {
 
 }
 
-void loop() {
-  digitalRead(button_1);
-  if (digitalRead(button_1) == HIGH) {
-    Serial.println("on");
-      // Fill along the length of the strip in various colors...
-  epilepsi(strip.Color(0,   0,   0), 0); // Red
-  epilepsi(strip.Color(  random(0,255), random(0,255), random(0,255)), 255); // Green
-  random_delay = random(10, 30);
-  } if (digitalRead(button_1) == LOW) {
-  epilepsi(strip.Color(0,   0,   0), 0);
-  }
-
-
-}
-
 void epilepsi(uint32_t color, int wait) {
   for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
   }
       strip.show();                          //  Update strip to match
       delay(random_delay);
+}
+
+void loop() {
+  digitalRead(Stage_1);
+  if (digitalRead(Stage_1) == LOW) {
+    Serial.println("on");
+      // Fill along the length of the strip in various colors...
+  epilepsi(strip.Color(0,   0,   0), 0); // Red
+  epilepsi(strip.Color(  random(0,255), random(0,255), random(0,255)), 255); // Green
+  random_delay = random(10, 30);
+  } if (digitalRead(Stage_1) == HIGH) {
+  epilepsi(strip.Color(0,   0,   0), 0);
+  }
+
+
 }
